e6a2cdae24b5ae5f421638b3c7c13cea2c35752b
SominZex <sominzex21@gmail.com>
2026-01-21 00:22:11 +0530

    update variables

--- etl/etl_pip.py
+++ etl/etl_pip.py
@@ -47,7 +47,7 @@ BIGINT_COLUMNS = ["barcode"]
 NUMERIC_COLUMNS = ["GST", "CGSTRate", "SGSTRate", "acessAmount", "cess"]
 
 class CSVDownloader:
-    def __init__(self, base_url="https://api.thenewshop.in", username="nssomin", password="nssomin"):
+    def __init__(self, base_url="https://api.example.in", username="user", password="pw"):
         self.base_url = base_url
         self.username = username
         self.password = password
