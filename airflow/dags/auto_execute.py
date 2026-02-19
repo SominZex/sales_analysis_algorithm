858effa51f2bec5361f9da4c008c9aabbe112cbb
SominZex <sominzex21@gmail.com>
2026-02-19 22:48:10 +0530

    update executor dag

--- airflow/dags/auto_execute.py
+++ airflow/dags/auto_execute.py
@@ -4,6 +4,17 @@ from airflow.operators.python import BranchPythonOperator
 from airflow.operators.empty import EmptyOperator
 from datetime import datetime
 
+from datetime import timedelta
+
+default_args = {
+    "owner": "Somin",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=7),
+    "email": ["sominzex@gmail.com"],
+    "email_on_failure": True,
+    "email_on_retry": False,
+}
+
 def check_weekly(**context):
     if context["logical_date"].weekday() == 0:
         return "weekly_reports"
@@ -17,6 +28,7 @@ def check_monthly(**context):
 with DAG(
     dag_id="sales_master_pipeline",
     start_date=datetime(2024, 1, 1),
+    default_args=default_args,
     schedule="25 0 * * *",
     catchup=False,
     max_active_runs=1,
@@ -27,22 +39,24 @@ with DAG(
 
     etl = BashOperator(
         task_id="etl_pip",
-        bash_command="/home/azureuser/etl/vmac/bin/python /home/azureuser/etl/etl_pip.py "
+        bash_command="/home/base/etl/vmac/bin/python /home/base/etl/etl_pip.py "
     )
 
     product_update = BashOperator(
         task_id="product_update",
-        bash_command="/home/azureuser/etl/vmac/bin/python /home/azureuser/etl/product_update.py "
+        bash_command="/home/base/etl/vmac/bin/python /home/base/etl/product_update.py "
     )
 
     analysis = BashOperator(
         task_id="daily_analysis",
-        bash_command="bash /home/azureuser/azure_analysis_algorithm/run_analysis.sh "
+        bash_command="bash /home/base/sales_analysis_algorithm/run_analysis.sh "
     )
 
     daily_whatsapp = BashOperator(
         task_id="daily_whatsapp",
-        bash_command="/home/azureuser/azure_analysis_algorithm/vmac/bin/python /home/azureuser/azure_analysis_algorithm/wa_sender.py "
+        retries=2,
+        retry_delay=timedelta(minutes=5),
+        bash_command="/home/base/sales_analysis_algorithm/vmac/bin/python /home/base/sales_analysis_algorithm/wa_sender.py "
     )
 
     # ---------------- WEEKLY ----------------
@@ -54,12 +68,14 @@ with DAG(
 
     weekly_reports = BashOperator(
         task_id="weekly_reports",
-        bash_command="/home/azureuser/azure_analysis_algorithm/vmac/bin/python /home/azureuser/azure_analysis_algorithm/weekly_reports.py "
+        bash_command="/home/base/sales_analysis_algorithm/vmac/bin/python /home/base/sales_analysis_algorithm/weekly_reports.py "
     )
 
     weekly_mail = BashOperator(
         task_id="weekly_mail",
-        bash_command="/home/azureuser/azure_analysis_algorithm/vmac/bin/python /home/azureuser/azure_analysis_algorithm/mail.py "
+        retries=2,
+        retry_delay=timedelta(minutes=5),
+        bash_command="/home/base/sales_analysis_algorithm/vmac/bin/python /home/base/sales_analysis_algorithm/mail.py "
     )
 
     skip_weekly = EmptyOperator(task_id="skip_weekly")
@@ -73,12 +89,14 @@ with DAG(
 
     monthly_reports = BashOperator(
         task_id="monthly_reports",
-        bash_command="/home/azureuser/azure_analysis_algorithm/vmac/bin/python /home/azureuser/azure_analysis_algorithm/monthly_reports.py "
+        bash_command="/home/base/sales_analysis_algorithm/vmac/bin/python /home/base/sales_analysis_algorithm/monthly_reports.py "
     )
 
     monthly_mail = BashOperator(
         task_id="monthly_mail",
-        bash_command="/home/azureuser/etl/vmac/bin/python /home/azureuser/azure_analysis_algorithm/monthly_mail.py "
+        retries=2,
+        retry_delay=timedelta(minutes=5),
+        bash_command="/home/base/etl/vmac/bin/python /home/base/sales_analysis_algorithm/monthly_mail.py "
     )
 
     skip_monthly = EmptyOperator(task_id="skip_monthly")
@@ -86,12 +104,14 @@ with DAG(
     # ---------------- DEPENDENCIES ----------------
 
     # Daily core chain
-    etl >> product_update >> analysis >> daily_whatsapp >> weekly_branch
+    etl >> product_update >> analysis >> daily_whatsapp
 
     # Weekly chain
-    weekly_branch >> weekly_reports >> weekly_mail >> monthly_branch
-    weekly_branch >> skip_weekly >> monthly_branch
+    daily_whatsapp >> weekly_branch
+    weekly_branch >> weekly_reports >> weekly_mail
+    weekly_branch >> skip_weekly
 
     # Monthly chain
+    daily_whatsapp >> monthly_branch
     monthly_branch >> monthly_reports >> monthly_mail
     monthly_branch >> skip_monthly
